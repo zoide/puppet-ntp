1b6a39866e054c32a1d1619dd1d45f1e966061e0
Udo Waechter <u.waechter@mysportgroup.de>
2013-08-22 14:01:45 +0200

    removing default node, it breaks puppetdoc generation

--- tests/init.pp
+++ tests/init.pp
@@ -1,11 +1,11 @@
-node default {
-
-  notify { 'enduser-before': }
-  notify { 'enduser-after': }
-
-  class { 'ntp':
-    require => Notify['enduser-before'],
-    before  => Notify['enduser-after'],
-  }
-
-}
+#node default {
+#
+#  notify { 'enduser-before': }
+#  notify { 'enduser-after': }
+#
+#  class { 'ntp':
+#    require => Notify['enduser-before'],
+#    before  => Notify['enduser-after'],
+#  }
+#
+#}
